deb348a353e4a78a81df3b04079de5826e08a587
wangxiaofei <wangxiaofei@corp.netease.com>
2017-02-23 16:33:02 +0800

    继续对lateral 缓存进行研究

--- src/org/apache/commons/jcs/auxiliary/lateral/socket/tcp/LateralTCPCacheManager.java
+++ src/org/apache/commons/jcs/auxiliary/lateral/socket/tcp/LateralTCPCacheManager.java
@@ -1,5 +1,10 @@
 package org.apache.commons.jcs.auxiliary.lateral.socket.tcp;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -37,11 +42,6 @@ import org.apache.commons.jcs.engine.logging.behavior.ICacheEventLogger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * Creates lateral caches. Lateral caches are primarily used for removing non laterally configured
  * caches. Non laterally configured cache regions should still be able to participate in removal.
@@ -52,304 +52,302 @@ import java.util.concurrent.ConcurrentHashMap;
  * <p>
  * TODO: - need freeCache, release, getStats - need to find an interface acceptable for all - cache
  * managers or a manager within a type
+ * 
  */
-public class LateralTCPCacheManager
-    extends LateralCacheAbstractManager
+public class LateralTCPCacheManager extends LateralCacheAbstractManager
 {
-    /** The logger. */
-    private static final Log log = LogFactory.getLog( LateralTCPCacheManager.class );
+	/** The logger. */
+	private static final Log log = LogFactory.getLog(LateralTCPCacheManager.class);
 
-    /** The monitor */
-    private static LateralCacheMonitor monitor;
+	/** The monitor */
+	private static LateralCacheMonitor monitor;
 
-    /** Address to instance map. */
-    private static final Map<String, LateralTCPCacheManager> instances =
-        new ConcurrentHashMap<String, LateralTCPCacheManager>();
+	/** Address to instance map. */
+	private static final Map<String, LateralTCPCacheManager> instances = new ConcurrentHashMap<String, LateralTCPCacheManager>();
 
-    /** ITCPLateralCacheAttributes */
-    private final ITCPLateralCacheAttributes lateralCacheAttributes;
+	/** ITCPLateralCacheAttributes */
+	private final ITCPLateralCacheAttributes lateralCacheAttributes;
 
-    /**
-     * Handle to the lateral cache service; or a zombie handle if failed to connect.
-     */
-    private ICacheServiceNonLocal<?, ?> lateralService;
+	/**
+	 * Handle to the lateral cache service; or a zombie handle if failed to connect.
+	 */
+	private ICacheServiceNonLocal<?, ?> lateralService;
 
-    /**
-     * Wrapper of the lateral cache watch service; or wrapper of a zombie service if failed to
-     * connect.
-     */
-    private final LateralCacheWatchRepairable lateralWatch;
+	/**
+	 * Wrapper of the lateral cache watch service; or wrapper of a zombie service if failed to
+	 * connect.
+	 */
+	private final LateralCacheWatchRepairable lateralWatch;
 
-    /** This is set in the constructor. */
-    private final ICompositeCacheManager cacheMgr;
+	/** This is set in the constructor. */
+	private final ICompositeCacheManager cacheMgr;
 
-    private final ICacheEventLogger cacheEventLogger;
+	private final ICacheEventLogger cacheEventLogger;
 
-    private final IElementSerializer elementSerializer;
+	private final IElementSerializer elementSerializer;
 
-    /**
-     * Returns an instance of the LateralCacheManager.
-     * <p>
-     * @param lca
-     * @param cacheMgr this allows the auxiliary to be passed a cache manager.
-     * @param cacheEventLogger
-     * @param elementSerializer
-     * @return LateralTCPCacheManager
-     */
-    public static LateralTCPCacheManager getInstance( ITCPLateralCacheAttributes lca, ICompositeCacheManager cacheMgr,
-                                                      ICacheEventLogger cacheEventLogger,
-                                                      IElementSerializer elementSerializer )
-    {
-        synchronized ( instances )
-        {
-            String key = lca.getTcpServer();
-            LateralTCPCacheManager ins = instances.get( key );
-            if ( ins == null )
-            {
-                log.info( "Instance for [" + key + "] is null, creating" );
+	/**
+	 * Returns an instance of the LateralCacheManager.
+	 * <p>
+	 * @param lca
+	 * @param cacheMgr this allows the auxiliary to be passed a cache manager.
+	 * @param cacheEventLogger
+	 * @param elementSerializer
+	 * @return LateralTCPCacheManager
+	 */
+	public static LateralTCPCacheManager getInstance(ITCPLateralCacheAttributes lca, ICompositeCacheManager cacheMgr,
+			ICacheEventLogger cacheEventLogger, IElementSerializer elementSerializer)
+	{
+		synchronized (instances)
+		{
+			String key = lca.getTcpServer();
+			LateralTCPCacheManager ins = instances.get(key);
+			if (ins == null)
+			{
+				log.info("Instance for [" + key + "] is null, creating");
 
-                ins = instances.get( lca.getTcpServer() );
-                if ( ins == null )
-                {
-                    ins = new LateralTCPCacheManager( lca, cacheMgr, cacheEventLogger, elementSerializer );
-                    instances.put( key, ins );
-                }
+				ins = instances.get(lca.getTcpServer());
+				if (ins == null)
+				{
+					ins = new LateralTCPCacheManager(lca, cacheMgr, cacheEventLogger, elementSerializer);
+					instances.put(key, ins);
+				}
 
-                createMonitor( ins );
-            }
+				createMonitor(ins);
+			}
 
-            return ins;
-        }
-    }
+			return ins;
+		}
+	}
 
-    /**
-     * The monitor needs reference to one instance, actually just a type.
-     * <p>
-     * TODO refactor this.
-     * <p>
-     * @param instance
-     */
-    private static synchronized void createMonitor( ILateralCacheManager instance )
-    {
-        // only want one monitor per lateral type
-        // Fires up the monitoring daemon.
-        if ( monitor == null )
-        {
-            monitor = new LateralCacheMonitor( instance );
-            Thread t = new Thread( monitor );
-            t.setDaemon( true );
-            t.start();
-        }
-    }
+	/**
+	 * The monitor needs reference to one instance, actually just a type.
+	 * <p>
+	 * TODO refactor this.
+	 * <p>
+	 * @param instance
+	 */
+	private static synchronized void createMonitor(ILateralCacheManager instance)
+	{
+		// only want one monitor per lateral type
+		// Fires up the monitoring daemon.
+		if (monitor == null)
+		{
+			monitor = new LateralCacheMonitor(instance);
+			Thread t = new Thread(monitor);
+			t.setDaemon(true);
+			t.start();
+		}
+	}
 
-    /**
-     * Constructor for the LateralCacheManager object.
-     * <p>
-     * @param lcaA
-     * @param cacheMgr
-     * @param cacheEventLogger
-     * @param elementSerializer
-     */
-    private LateralTCPCacheManager( ITCPLateralCacheAttributes lcaA, ICompositeCacheManager cacheMgr,
-                                    ICacheEventLogger cacheEventLogger, IElementSerializer elementSerializer )
-    {
-        this.lateralCacheAttributes = lcaA;
-        this.cacheMgr = cacheMgr;
-        this.cacheEventLogger = cacheEventLogger;
-        this.elementSerializer = elementSerializer;
+	/**
+	 * Constructor for the LateralCacheManager object.
+	 * <p>
+	 * @param lcaA
+	 * @param cacheMgr
+	 * @param cacheEventLogger
+	 * @param elementSerializer
+	 */
+	private LateralTCPCacheManager(ITCPLateralCacheAttributes lcaA, ICompositeCacheManager cacheMgr,
+			ICacheEventLogger cacheEventLogger, IElementSerializer elementSerializer)
+	{
+		this.lateralCacheAttributes = lcaA;
+		this.cacheMgr = cacheMgr;
+		this.cacheEventLogger = cacheEventLogger;
+		this.elementSerializer = elementSerializer;
 
-        this.lateralWatch = new LateralCacheWatchRepairable();
-        this.lateralWatch.setCacheWatch( new ZombieLateralCacheWatch() );
+		this.lateralWatch = new LateralCacheWatchRepairable();
+		this.lateralWatch.setCacheWatch(new ZombieLateralCacheWatch());
 
-        if ( log.isDebugEnabled() )
-        {
-            log.debug( "Creating lateral cache service, lca = " + this.lateralCacheAttributes );
-        }
+		if (log.isDebugEnabled())
+		{
+			log.debug("Creating lateral cache service, lca = " + this.lateralCacheAttributes);
+		}
 
-        // Create the service
-        try
-        {
-            if ( log.isInfoEnabled() )
-            {
-                log.info( "Creating TCP service, lca = " + this.lateralCacheAttributes );
-            }
+		// Create the service
+		try
+		{
+			if (log.isInfoEnabled())
+			{
+				log.info("Creating TCP service, lca = " + this.lateralCacheAttributes);
+			}
 
-            this.lateralService = new LateralTCPService<Serializable, Serializable>( this.lateralCacheAttributes );
-        }
-        catch ( Exception ex )
-        {
-            // Failed to connect to the lateral server.
-            // Configure this LateralCacheManager instance to use the
-            // "zombie" services.
-            log.error( "Failure, lateral instance will use zombie service", ex );
+			this.lateralService = new LateralTCPService<Serializable, Serializable>(this.lateralCacheAttributes);
+		}
+		catch (Exception ex)
+		{
+			// Failed to connect to the lateral server.
+			// Configure this LateralCacheManager instance to use the
+			// "zombie" services.
+			log.error("Failure, lateral instance will use zombie service", ex);
 
-            this.lateralService = new ZombieCacheServiceNonLocal<Serializable, Serializable>( lateralCacheAttributes.getZombieQueueMaxSize() );
+			this.lateralService = new ZombieCacheServiceNonLocal<Serializable, Serializable>(
+					lateralCacheAttributes.getZombieQueueMaxSize());
 
-            // Notify the cache monitor about the error, and kick off
-            // the recovery process.
-            createMonitor( this );
-            monitor.notifyError();
-        }
-    }
+			// Notify the cache monitor about the error, and kick off
+			// the recovery process.
+			createMonitor(this);
+			monitor.notifyError();
+		}
+	}
 
-    /**
-     * Adds the lateral cache listener to the underlying cache-watch service.
-     * <p>
-     * @param cacheName The feature to be added to the LateralCacheListener attribute
-     * @param listener The feature to be added to the LateralCacheListener attribute
-     * @throws IOException
-     */
-    @Override
-    public <K, V> void addLateralCacheListener( String cacheName, ILateralCacheListener<K, V> listener )
-        throws IOException
-    {
-        synchronized ( this.caches )
-        {
-            lateralWatch.addCacheListener( cacheName, listener );
-        }
-    }
+	/**
+	 * Adds the lateral cache listener to the underlying cache-watch service.
+	 * <p>
+	 * @param cacheName The feature to be added to the LateralCacheListener attribute
+	 * @param listener The feature to be added to the LateralCacheListener attribute
+	 * @throws IOException
+	 */
+	@Override
+	public <K, V> void addLateralCacheListener(String cacheName, ILateralCacheListener<K, V> listener)
+			throws IOException
+	{
+		synchronized (this.caches)
+		{
+			lateralWatch.addCacheListener(cacheName, listener);
+		}
+	}
 
-    /**
-     * Called to access a precreated region or construct one with defaults. Since all aux cache
-     * access goes through the manager, this will never be called.
-     * <p>
-     * After getting the manager instance for a server, the factory gets a cache for the region name
-     * it is constructing.
-     * <p>
-     * There should be one manager per server and one cache per region per manager.
-     * <p>
-     * @return AuxiliaryCache
-     * @param cacheName
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public <K, V> LateralCacheNoWait<K, V> getCache( String cacheName )
-    {
-        LateralCacheNoWait<K, V> lateralNoWait = null;
-        synchronized ( caches )
-        {
-            // Need to cast because of common map for all caches
-            lateralNoWait = (LateralCacheNoWait<K, V>) caches.get( cacheName );
-            if ( lateralNoWait == null )
-            {
-                LateralCacheAttributes attr = (LateralCacheAttributes) lateralCacheAttributes.copy();
-                attr.setCacheName( cacheName );
+	/**
+	 * Called to access a precreated region or construct one with defaults. Since all aux cache
+	 * access goes through the manager, this will never be called.
+	 * <p>
+	 * After getting the manager instance for a server, the factory gets a cache for the region name
+	 * it is constructing.
+	 * <p>
+	 * There should be one manager per server and one cache per region per manager.
+	 * <p>
+	 * @return AuxiliaryCache
+	 * @param cacheName
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public <K, V> LateralCacheNoWait<K, V> getCache(String cacheName)
+	{
+		LateralCacheNoWait<K, V> lateralNoWait = null;
+		synchronized (caches)
+		{
+			// Need to cast because of common map for all caches
+			lateralNoWait = (LateralCacheNoWait<K, V>) caches.get(cacheName);
+			if (lateralNoWait == null)
+			{
+				LateralCacheAttributes attr = (LateralCacheAttributes) lateralCacheAttributes.copy();
+				attr.setCacheName(cacheName);
 
-                // Need to cast to specific type
-                LateralCache<K, V> cache = new LateralCache<K, V>( attr,
-                        (ICacheServiceNonLocal<K, V>)this.lateralService, monitor );
-                cache.setCacheEventLogger( cacheEventLogger );
-                cache.setElementSerializer( elementSerializer );
+				// Need to cast to specific type
+				LateralCache<K, V> cache = new LateralCache<K, V>(attr,
+						(ICacheServiceNonLocal<K, V>) this.lateralService, monitor);
+				cache.setCacheEventLogger(cacheEventLogger);
+				cache.setElementSerializer(elementSerializer);
 
-                if ( log.isDebugEnabled() )
-                {
-                    log.debug( "Created cache for noWait, cache [" + cache + "]" );
-                }
+				if (log.isDebugEnabled())
+				{
+					log.debug("Created cache for noWait, cache [" + cache + "]");
+				}
 
-                lateralNoWait = new LateralCacheNoWait<K, V>( cache );
-                lateralNoWait.setCacheEventLogger( cacheEventLogger );
-                lateralNoWait.setElementSerializer( elementSerializer );
+				lateralNoWait = new LateralCacheNoWait<K, V>(cache);
+				lateralNoWait.setCacheEventLogger(cacheEventLogger);
+				lateralNoWait.setElementSerializer(elementSerializer);
 
-                caches.put( cacheName, lateralNoWait );
+				caches.put(cacheName, lateralNoWait);
 
-                if ( log.isInfoEnabled() )
-                {
-                    log.info( "Created LateralCacheNoWait for [" + lateralCacheAttributes + "] LateralCacheNoWait = [" + lateralNoWait
-                        + "]" );
-                }
+				if (log.isInfoEnabled())
+				{
+					log.info("Created LateralCacheNoWait for [" + lateralCacheAttributes + "] LateralCacheNoWait = ["
+							+ lateralNoWait + "]");
+				}
 
-                // this used to be called every getCache. i move it in here.
-                addListenerIfNeeded( cacheName );
-            }
-        }
+				// this used to be called every getCache. i move it in here.
+				addListenerIfNeeded(cacheName);
+			}
+		}
 
-        return lateralNoWait;
-    }
+		return lateralNoWait;
+	}
 
-    /**
-     * TODO see if this belongs here or in the factory.
-     * <p>
-     * @param cacheName
-     */
-    private void addListenerIfNeeded( String cacheName )
-    {
-        // don't create a listener if we are not receiving.
-        if ( lateralCacheAttributes.isReceive() )
-        {
-            try
-            {
-                addLateralCacheListener( cacheName, LateralTCPListener.getInstance( lateralCacheAttributes, cacheMgr ) );
-            }
-            catch ( IOException ioe )
-            {
-                log.error( "Problem creating lateral listener", ioe );
-            }
-            catch ( Exception e )
-            {
-                log.error( "Problem creating lateral listener", e );
-            }
-        }
-        else
-        {
-            if ( log.isDebugEnabled() )
-            {
-                log.debug( "Not creating a listener since we are not receiving." );
-            }
-        }
-    }
+	/**
+	 * TODO see if this belongs here or in the factory.
+	 * <p>
+	 * @param cacheName
+	 */
+	private void addListenerIfNeeded(String cacheName)
+	{
+		// don't create a listener if we are not receiving.
+		if (lateralCacheAttributes.isReceive())
+		{
+			try
+			{
+				addLateralCacheListener(cacheName, LateralTCPListener.getInstance(lateralCacheAttributes, cacheMgr));
+			}
+			catch (IOException ioe)
+			{
+				log.error("Problem creating lateral listener", ioe);
+			}
+			catch (Exception e)
+			{
+				log.error("Problem creating lateral listener", e);
+			}
+		}
+		else
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("Not creating a listener since we are not receiving.");
+			}
+		}
+	}
 
-    /**
-     * @return Map
-     */
-    @Override
-    public Map<String, ? extends ILateralCacheManager> getInstances()
-    {
-        return instances;
-    }
+	/**
+	 * @return Map
+	 */
+	@Override
+	public Map<String, ? extends ILateralCacheManager> getInstances()
+	{
+		return instances;
+	}
 
-    /**
-     * @return a new service
-     * @throws IOException
-     */
-    @Override
-    public Object fixService()
-        throws IOException
-    {
-        Object service = null;
-        try
-        {
-            service = new LateralTCPService<Serializable, Serializable>( lateralCacheAttributes );
-        }
-        catch ( Exception ex )
-        {
-            log.error( "Can't fix " + ex.getMessage() );
-            throw new IOException( "Can't fix " + ex.getMessage() );
-        }
-        return service;
-    }
+	/**
+	 * @return a new service
+	 * @throws IOException
+	 */
+	@Override
+	public Object fixService() throws IOException
+	{
+		Object service = null;
+		try
+		{
+			service = new LateralTCPService<Serializable, Serializable>(lateralCacheAttributes);
+		}
+		catch (Exception ex)
+		{
+			log.error("Can't fix " + ex.getMessage());
+			throw new IOException("Can't fix " + ex.getMessage());
+		}
+		return service;
+	}
 
-    /**
-     * Shuts down the lateral sender. This does not shutdown the listener. This can be called if the
-     * end point is taken out of service.
-     */
-    @Override
-    public void shutdown()
-    {
-        // TODO revisit this.
-        // the name here doesn't matter.
-        try
-        {
-            lateralService.dispose( "ALL" );
-        }
-        catch ( IOException e )
-        {
-            log.error( "Problem disposing of service", e );
-        }
+	/**
+	 * Shuts down the lateral sender. This does not shutdown the listener. This can be called if the
+	 * end point is taken out of service.
+	 */
+	@Override
+	public void shutdown()
+	{
+		// TODO revisit this.
+		// the name here doesn't matter.
+		try
+		{
+			lateralService.dispose("ALL");
+		}
+		catch (IOException e)
+		{
+			log.error("Problem disposing of service", e);
+		}
 
-        // shut down monitor
-        if (monitor != null)
-        {
-            monitor.notifyShutdown();
-        }
-    }
+		// shut down monitor
+		if (monitor != null)
+		{
+			monitor.notifyShutdown();
+		}
+	}
 }
